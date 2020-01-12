a0fa85c1db902c327945d503d5d4c080276b9a85
avnerkhan <avnerkhan@utexas.edu>
2020-01-11 18:30:26 -0600

    More URLs

--- scrappers/Main/Main/spiders/PullUpSpider.py
+++ scrappers/Main/Main/spiders/PullUpSpider.py
@@ -3,16 +3,11 @@ import scrapy
 from selenium import webdriver
 
 
-# For this page, we get other pages by put start=page_number*100. (start=100, start=200, etc)
-def generate_other_starts(page_count):
-    return ["https://www.123rf.com/stock-photo/pull_up_workout.html?oriSearch=pull%20up&start={}++&sti=muxocqqycvj9o9fzvv|".format(i*100) for i in range(1, page_count)]
-
-
 class PullUpSpider(scrapy.Spider):
     name = "pull-up-image-spider"
     # First page does not follow start=page_number*100 rule
     start_urls = [
-        "https://www.123rf.com/stock-photo/pull_up_workout.html?oriSearch=pull%20up&sti=muxocqqycvj9o9fzvv|", *generate_other_starts(42)]
+        "https://www.123rf.com/stock-photo/pull_up_workout.html?oriSearch=pull+up+workout|"]
 
     def parse(self, response):
         # Getting source from all image tags
@@ -21,3 +16,6 @@ class PullUpSpider(scrapy.Spider):
             if img_src:
                 imageURL = img_src.extract_first()
                 yield MainItem(file_urls=[imageURL])
+
+        next_button = response.xpath("//*[@id='btn_main_nextpg']")
+        yield scrapy.Request(next_button.xpath("@href").extract_first(), self.parse)
